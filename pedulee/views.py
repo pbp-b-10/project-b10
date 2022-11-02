74708b800ee83bc1ec08d49d9e12eb0dbe8a2c01
Anon <anon@anon.com>
2022-11-02 11:08:26 +0700

    Update Project + Volunteer

--- pedulee/views.py
+++ pedulee/views.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from multiprocessing import context
 from django.shortcuts import render
 from django.shortcuts import redirect
@@ -14,18 +15,20 @@ from django.core import serializers
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .forms import ProfileForm
-from .models import Cloth, Volunteer, Money
+from .models import Cloth, Volunteer, Money, Project
 
-# Create your views here.
+def my_render(request, page, context:dict):
+    context['username'] = request.user
+    return render(request, page, context)
 
 class HomeViews:
     @staticmethod
     def index(request):
-        username = request.user
+        projects = Project.objects.all()
         context = {
-            'username': username,
+            'projects': projects,
         }
-        return render(request, "home.html", context)
+        return my_render(request, "home.html", context)
 
 class UserViews:
     @staticmethod
@@ -50,7 +53,6 @@ class UserViews:
         else:
             form = ExtendedUserCreationForm()
             profile_form = ProfileForm()
-
 
         context = {'form':form, 'profile_form':profile_form}
         return render(request, 'signup.html', context)
@@ -100,7 +102,6 @@ class HistoryView:
             'username': username,
         }
         return render(request, "history.html", context)
-
 
     @staticmethod
     @login_required(login_url="/sign-in")
@@ -156,9 +157,11 @@ class HistoryView:
 class ProjectView:
     @staticmethod
     def show(request):
-        context = {}
-        return render(request, 'projects/index.html', context)
-
+        projects = Project.objects.all()
+        context = {
+            'projects': projects,
+        }
+        return my_render(request, 'projects/index.html', context)
 
 class ClothesView:
     @staticmethod
@@ -234,27 +237,50 @@ class ClothesView:
             cloth.delete()
         return HttpResponse(b"DELETE")
 
-
 class VolunteerView:
     @staticmethod
+    @login_required(login_url="/sign-in")
     def create(request):
         form = VolunteerForm()
         if request.method == 'POST':
             form = VolunteerForm(request.POST)
-
+            print(form)
             if form.is_valid():
+                print('isvalid')
                 volunteer = form.save(commit=False)
                 volunteer.user = request.user
                 volunteer.save()
                 return HttpResponse(b"CREATED", status=201)
 
-        context = {'form': form, 'username': request.user}
-        return render(request, 'volunteer/form.html', context)
+        projects = Project.objects.all()
+        context = {
+            'form': form,
+            'projects': serializers.serialize('json', projects)
+        }
+        return my_render(request, 'volunteer/form.html', context)
 
     @staticmethod
     def show_json(request):
         data = Volunteer.objects.filter(user = request.user).prefetch_related('project')
-        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+        response = []
+        for item in data:
+            response.append({
+                'id': item.pk,
+                'title': item.project.title,
+                'amount': item.project.amount,
+                'divisi': item.divisi
+            })
+            print(item.project.title, item.project.amount)
+        return HttpResponse(json.dumps(response), content_type="application/json")
+    
+    @staticmethod
+    @csrf_exempt
+    def delete (request, i):
+        print('deleting ', i)
+        if request.method == "DELETE":
+            volunteer = Volunteer.objects.get(id=i)
+            volunteer.delete()
+        return HttpResponse(b"DELETE")
 
 class MoneyView:
     @staticmethod
